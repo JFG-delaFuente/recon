c18fe1d1ffd80a86e063589d4012788e38c68a3c
JFG-delaFuente <117809094+JFG-delaFuente@users.noreply.github.com>
2023-09-11 17:46:10 +0200

    Update reflectedxss.sh

--- reflectedxss.sh
+++ reflectedxss.sh
@@ -2,18 +2,21 @@
 
 urls="$(pwd)/urls.txt"
 poc="$(pwd)/xss.txt"
+templates="/root/nuclei-templates/reflected-xss"
 
-# Llamamos a Dalfox 
-echo "Empezamos a probar XSS con dalfox"
-for line in $urls
+# Llamamos a nuclei para probar XSS
+echo "Empezamos a escanear los XSS con nuclei"
+for line in $(cat $urls)
 do
-  dalfox url "$line" -o xss.txt
-done 
-
-# Llamamos a xsstrike
-echo "Empezamos a escanear con xsstrike"
-for line in $urls
-do
-  python3 /root/tools/XSStrike/xsstrike.py -u "$line" --crawl -l 3 >> $poc
+  nuclei -u "$line" -t $templates
 done
+
+# Llamamos a Dalfox
+echo "Empezamos a probar XSS con dalfox"
+for line in $(cat $urls)
+do
+  echo $urls, $line
+  dalfox url "$line" -o xss.txt
+done
+
 
